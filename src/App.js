21095c3f54f212eda5f644025e4b30cf8dad75d9
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-10-19 19:46:14 +0100

    sass style attached

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import AcademyPage from './pages/Academy';
 import CommunityPage from './pages/Community';
 import GetStartedPage from './pages/GetStarted';
 import Header from './components/layout/Header';
+import "./scss/style.scss"
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
-    <Route path='/' exact>
-      <HomePage />
-    </Route>
-    <Route path='/academy'>
-      <AcademyPage />
-    </Route>
-    <Route path='/community'>
-      <CommunityPage />
-    </Route>
-    <Route path='/get-started'>
-      <GetStartedPage />
-    </Route>
-    </Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/academy'>
+          <AcademyPage />
+        </Route>
+        <Route path='/community'>
+          <CommunityPage />
+        </Route>
+        <Route path='/get-started'>
+          <GetStartedPage />
+        </Route>
+      </Switch>
+      <div style={{ fontSize: "48px", color: "red" }}>Ademola</div>
     </div>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,27 @@
 import { Link } from 'react-router-dom'
-import classes from './Header.module.css'
+import './Header.module.css'
+import logo from '../../img/logo-no-back-01-1536x531.png'
 function Header() {
-    return<header>
-<div class="container">
-    
-    <Link to='/'>
-        <img src="img/logo-no-back-01-1536x531.png" alt="LOgo" />
-        </Link>
-
-    <i class="fa fa-bars" aria-hidden="true"></i>
-    <i class="fa fa-times" aria-hidden="true"></i>
-
-    <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/academy'>Academy</Link>
-        <Link to='/community'>Community</Link>
-    </nav>
-    <Link name="#" id="" class="btn btn-primary" role="button" to='/get-started'> Get Started </Link>
-</div>
+    return <header className='header'>
+            <Link to='/'>
+                <img src={logo} alt="Logo" className='logo' />
+            </Link>
+            <nav className="nav">
+                <Link to='/'>Home</Link>
+                <Link to='/academy'>Academy</Link>
+                <Link to='/community'>Community</Link>
+            </nav>
+            <Link name="#" id="" className="btn btn-green" role="button" to='/get-started'> Get Started </Link>
+            <span className='hamburger'>
+                <i className='closebtn fa-times'>&times;</i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="28" aria-hidden="true"
+                    focusable="false" data-prefix="fas" data-icon="bars" className="openNav svg-inline--fa fa-bars fa-w-14" role="img"
+                    viewBox="0 0 448 512">
+                    <path fill="white"
+                        d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
+                </svg>
+            </span>
     </header>
+
 }
 export default Header;
