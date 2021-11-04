dfec4a1db66fd215996aafdb25b3677dee4ecd42
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-11-04 10:26:20 +0100

    get started page done

--- src/pages/GetStarted.js
+++ src/pages/GetStarted.js
@@ -2,14 +2,14 @@ function GetStartedPage() {
     return <div>
         <section className='getStarted'>
             <div>
-                <h3> You deserve <span> financial freedom! </span> </h3>
-                <p>Get started here.</p>
+                <h1> You deserve <span> financial freedom! </span> </h1>
+                <h1>Get started here.</h1>
             </div>
             <form>
                 <input placeholder='First Name' required />
                 <input placeholder='Last Name' required/>
                 <input placeholder='Email' required/>
-                <button>Get Started</button>
+                <button>Get Started Now</button>
             </form>
         </section>
     </div>
